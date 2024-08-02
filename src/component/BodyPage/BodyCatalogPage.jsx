9cdb699a6dfeb2e804a3dcab28bb0ad69b5a408c
master <gubinvs@gmail.com>
2024-08-02 15:07:07 +0300

    edit

--- src/component/BodyPage/BodyCatalogPage.jsx
+++ src/component/BodyPage/BodyCatalogPage.jsx
@@ -3,6 +3,8 @@ import "./body.catalog.page.css";
 import TitlePage from "./TitlePage/TitlePage";
 import SearchBlock from "./TitlePage/SearchBlock";
 import FilterBlock from "./FilterBlock/FilterBlock"
+import { TableComponentBody, TableComponentTitle } from "./TableComponent/TableComponent";
+
 
 function BodyCatalogPage (props) {
 
@@ -13,6 +15,8 @@ function BodyCatalogPage (props) {
             <SearchBlock />
           </div>
           <FilterBlock />
+          <TableComponentTitle />
+          <TableComponentBody />
         </>
     )
 
--- src/component/BodyPage/TableComponent/TableComponent.jsx
+++ src/component/BodyPage/TableComponent/TableComponent.jsx
@@ -1,14 +1,50 @@
+import "./table.component.css"
 
 
-function TableComponent () {
+export function TableComponentTitle () {
     return (
         <>
-        <div>
-            
+        <div className="table-component-result table-component-result_title">
+            <div className="table-component-result__heading">
+                <div className="table-component-result__heading_left">
+                    <input type="checkbox" className="tcrh__check"></input>
+                    <img src="images/img-icon.svg" alt="Картинка" className="tcrh__img"></img>
+                    <div className="tcrh__vendor">Артикул</div>
+                    <div className="tcrh__name">Наименование комплектующих</div>
+                </div>
+                <div className="table-component-result__heading_right">
+                    <div className="tcrh__availability">Наличие</div>
+                    <div className="tcrh__price">Цена, ед</div>
+                    <div className="tcrh__count"></div> 
+                    <div className="tcrh__button"></div>     
+                </div>
+            </div>
         </div>
-
         </>
     )
 }
 
-export default TableComponent;
+export function TableComponentBody () {
+    return (
+            <div className="table-component-result">
+                <div className="table-component-result__heading">
+                    <div className="table-component-result__heading_left">
+                        <input type="checkbox" className="tcrh__check"></input>
+                        <img src="" alt="Картинка" className="tcrh__img"></img>
+                        <div className="tcrh__vendor"></div>
+                        <div className="tcrh__name"></div>
+                    </div>
+                    <div className="table-component-result__heading_right">
+                        <div className="tcrh__availability w-70px">10 000 шт.</div>
+                        <div className="tcrh__price w-70px">999 999 р.</div>
+                        <div className="tcrh__count">
+                            <div className="tcrh-count__decrement">-</div>
+                            <div className="tcrh-count__body">0</div>
+                            <div className="tcrh-count__increment">+</div>
+                        </div> 
+                        <button className="tcrh__button" id="tcrh__button">Добавить</button>     
+                    </div>
+                </div>
+            </div>
+    )
+}
