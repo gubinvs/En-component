b574fc3f30c3c4ab89322e9769093e052c4612f4
master <gubinvs@gmail.com>
2024-08-03 09:25:14 +0300

    create item

--- src/component/BodyPage/TableComponent/TableComponent.jsx
+++ src/component/BodyPage/TableComponent/TableComponent.jsx
@@ -7,7 +7,7 @@ export function TableComponentTitle () {
         <div className="table-component-result table-component-result_title">
             <div className="table-component-result__heading">
                 <div className="table-component-result__heading_left">
-                    <input type="checkbox" className="tcrh__check"></input>
+                    <input type="checkbox" className="tcrh__check" id="tcrh__check_all"></input>
                     <img src="images/img-icon.svg" alt="Картинка" className="tcrh__img"></img>
                     <div className="tcrh__vendor w-120px">Артикул</div>
                     <div className="tcrh__name">Наименование</div>
@@ -15,7 +15,7 @@ export function TableComponentTitle () {
                 <div className="table-component-result__heading_right">
                     <div className="tcrh__availability w-70px">Наличие</div>
                     <div className="tcrh__price w-70px">Цена, ед</div>
-                    <div className="tcrh__count"></div> 
+                    <div className="tcrh__count">Кол-во</div> 
                     <div className="tcrh__button"></div>     
                 </div>
             </div>
@@ -27,26 +27,23 @@ export function TableComponentTitle () {
 
 export function TableComponentBody (props) {
     const comp = props.item;
-    
     return (
         comp.map((item) =>
             <>            
                 <div className="table-component-result">
                     <div className="table-component-result__heading">
                         <div className="table-component-result__heading_left">
-                            <input type="checkbox" className="tcrh__check"></input>
-                            <img src={item.images} alt="Картинка" className="tcrh__img"></img>
+                            <input type="checkbox" className="tcrh__check" id="tcrh__check" key={item.id}></input>
+                            <a href={item.images} target="blank">
+                                <img src={item.images} alt="Картинка" className="tcrh__img"></img>
+                            </a>
                             <div className="tcrh__vendor w-120px">{item.vendor}</div>
                             <div className="tcrh__name">{item.name}</div>
                         </div>
                         <div className="table-component-result__heading_right">
                             <div className="tcrh__availability w-70px">{item.availability} шт.</div>
                             <div className="tcrh__price w-70px">{Intl.NumberFormat("ru", {style: "currency", currency: "RUB"}).format(parseFloat(item.price))}</div>
-                            <div className="tcrh__count">
-                                <button className="tcrh-count__decrement">-</button>
-                                <div className="tcrh-count__body">0</div>
-                                <button className="tcrh-count__increment">+</button>
-                            </div> 
+                            <input type="number" className="tcrh__count tcrh-count__body"></input>
                             <button className="tcrh__button" id="tcrh__button">Добавить</button>     
                         </div>
                     </div>
