69e41c9926a7e162ffbb1469077fdf19c236e320
master <gubinvs@gmail.com>
2024-08-09 09:39:41 +0300

    crete fun

--- src/component/BodyPage/TableComponent/TableComponent.jsx
+++ src/component/BodyPage/TableComponent/TableComponent.jsx
@@ -55,10 +55,23 @@ export function TableComponentBody (props) {
 function CheckAll () {
     const checkAll = document.getElementById('tcrh__check_all');
     const ckeckItem = document.querySelectorAll('.tcrh__check');    
-
-    ckeckItem.forEach(element => {
-        element.checked = checkAll.checked;
-    })
+    let count = 1;
+    if (checkAll.checked == true) {
+        ckeckItem.forEach(element => {
+                element.checked = checkAll.checked;
+                document.getElementById("tcrh__button_" + count).className = "tcrh__button tcrh__button_add-value";
+                document.getElementById("tcrh__count_" + count).value = 1;
+                count++;
+        })
+    } else {
+        ckeckItem.forEach(element => {
+            element.checked = checkAll.checked;
+            document.getElementById("tcrh__button_" + count).className = "tcrh__button";
+            document.getElementById("tcrh__count_" + count).value = 0;
+            count++;
+        })
+    }
+ 
 }
 
 // Функция обнуляет input с количеством товара в строке таблицы при изменении статуса checkbox в строке товара и изменяет кнопку добавить
