96ee42e7472e88f23315a113d2a47825326c4381
master <gubinvs@gmail.com>
2024-08-13 12:04:29 +0300

    d

--- src/component/BodyPage/TableComponent/TableComponent.jsx
+++ src/component/BodyPage/TableComponent/TableComponent.jsx
@@ -1,8 +1,6 @@
 import "./table.component.css"
-import LocalStorageAddBasketItem from "./LocalStorage";
 import CheckAll from "./CheckAll";
-import ClickCheck from "./ClickCheck";
-import CheckTheBox from "./CheckTheBox";
+import TableComponentRow from "./TableComponentRow";
 
 
 
@@ -33,41 +31,5 @@ export function TableComponentTitle () {
 export function TableComponentBody (props) {
     const comp = props.item; 
     countItem = Number(comp.at(-1).id);// получение последнего элемента массива
-    return (
-        comp.map((item) =>
-            <>            
-                <div className="table-component-result">
-                    <div className="table-component-result__heading">
-                        <div className="table-component-result__heading_left">
-                            <input onClick={() => ClickCheck(item.id)} type="checkbox" className="tcrh__check" id={"tcrh__check_" + item.id}></input>
-                            <a href={item.images} target="blank">
-                                <img src={item.images} alt="Картинка" className="tcrh__img"></img>
-                            </a>
-                            <div className="tcrh__vendor w-120px" id={"tcrh__vendor_" + item.id}>{item.vendor}</div>
-                            <div className="tcrh__name" id={"tcrh__name_" + item.id}>{item.name}</div>
-                        </div>
-                        <div className="table-component-result__heading_right">
-                            <div className="tcrh__availability w-70px">{item.availability} шт.</div>
-                            <div className="tcrh__price w-70px" id={"tcrh__price_" + item.id}>{Intl.NumberFormat("ru", {style: "currency", currency: "RUB"}).format(parseFloat(item.price))}</div>
-                            <input onClick={() => CheckTheBox(item.id)} type="number" min="0" className="tcrh__count tcrh-count__body" id={"tcrh__count_" + item.id}></input>
-                            <button onClick={()=>  ButtonOnClick(item.id)} className="tcrh__button" id={"tcrh__button_" + item.id}>Добавить</button>     
-                        </div>
-                    </div>
-                </div>
-            </>
-            )
-        )
-}
-
-
-// При нажатии на кнопку добавить, меняется цвет кнопки и надпись "в корзине"
-// Информация о данном товаре добавляется в localStorage
-// Не перемещать в отдельный файл, используется глобальная переменная countItem
-function ButtonOnClick (count) {
-    if (document.getElementById("tcrh__count_" + count).value != 0) {
-        const buttonItem = document.getElementById("tcrh__button_" + count);
-        buttonItem.className = "tcrh__button tcrh__button_add-basket";
-        buttonItem.innerHTML = "В корзине";
-        LocalStorageAddBasketItem(countItem);  
-    }
+    return (comp.map((item) =><TableComponentRow row={item}/>))
 }
---
+++ src/component/BodyPage/TableComponent/TableComponentRow.jsx
@@ -0,0 +1,44 @@
+import LocalStorageAddBasketItem from "./LocalStorage";
+import ClickCheck from "./ClickCheck";
+import CheckTheBox from "./CheckTheBox";
+
+
+function TableComponentRow (props) {
+const row = props.row;
+    return (
+        <>            
+            <div className="table-component-result">
+                <div className="table-component-result__heading">
+                    <div className="table-component-result__heading_left">
+                        <input onClick={() => ClickCheck(row.id)} type="checkbox" className="tcrh__check" id={"tcrh__check_" + row.id}></input>
+                        <a href={row.images} target="blank">
+                            <img src={row.images} alt="Картинка" className="tcrh__img"></img>
+                        </a>
+                        <div className="tcrh__vendor w-120px" id={"tcrh__vendor_" + row.id}>{row.vendor}</div>
+                        <div className="tcrh__name" id={"tcrh__name_" + row.id}>{row.name}</div>
+                    </div>
+                    <div className="table-component-result__heading_right">
+                        <div className="tcrh__availability w-70px">{row.availability} шт.</div>
+                        <div className="tcrh__price w-70px" id={"tcrh__price_" + row.id}>{Intl.NumberFormat("ru", {style: "currency", currency: "RUB"}).format(parseFloat(row.price))}</div>
+                        <input onClick={() => CheckTheBox(row.id)} type="number" min="0" className="tcrh__count tcrh-count__body" id={"tcrh__count_" + row.id}></input>
+                        <button onClick={()=>  ButtonOnClick(row.id)} className="tcrh__button" id={"tcrh__button_" + row.id}>Добавить</button>     
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default TableComponentRow;
+
+// При нажатии на кнопку добавить, меняется цвет кнопки и надпись "в корзине"
+// Информация о данном товаре добавляется в localStorage
+// Не перемещать в отдельный файл, используется глобальная переменная countItem
+function ButtonOnClick (count) {
+    if (document.getElementById("tcrh__count_" + count).value != 0) {
+        const buttonItem = document.getElementById("tcrh__button_" + count);
+        buttonItem.className = "tcrh__button tcrh__button_add-basket";
+        buttonItem.innerHTML = "В корзине";
+        LocalStorageAddBasketItem(countItem);  
+    }
+}
