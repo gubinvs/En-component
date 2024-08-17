93a5254b23c6107c2d72faa1f62896f3533ffcb1
master <gubinvs@gmail.com>
2024-08-17 17:14:53 +0300

    d

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './component/Header/Header'
 import SideNav from './component/SideNav/SideNav'
